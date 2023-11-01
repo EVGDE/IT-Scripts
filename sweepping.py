281cfeb87a30a202ba8004659dac4bddb09949ed
EVGDE <jenyadmev@gmail.com>
2023-11-01 14:59:17 +0300

    Modify2

--- sweepping.py
+++ sweepping.py
@@ -8,14 +8,15 @@ def get_domain_name(ip_address):
     except socket.herror:
         return "No domain name found"
 
-f = open('IpAdress.txt', 'w')
+f = open('list.txt', 'w')
 
 IP = input("[+] Enter the host IP address:\t")
 print("[+] Starting Ping Sweeper " + IP)
 dot = IP.rfind(".")
 IP = IP[0:dot + 1]
+f = open(IP + '0' + '.txt', 'w')
 
-for i in range(1, 255):
+for i in range(1, 10):
     host = IP + str(i)
     response = os.system("ping -c 1 -w 1 " + host + " >/dev/null")
     domain_name = get_domain_name(host)
@@ -26,4 +27,5 @@ for i in range(1, 255):
         print(host + " is down " + domain_name)
         f.write(host + " is down " + domain_name + "\n")
 f.close()
+
 
